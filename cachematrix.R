bbab3d7fed8b5028942fd76466cfb8f6a3273e90
Virender Singh Jaiprakash <virendersingh1982@yahoo.com>
2014-07-24 17:27:02 +0530

    Committing Code In Local Repository

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	cinv <- NULL
+    set <- function(y) {
+        x <<- y
+        cinv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) cinv <<- inverse
+    getinverse <- function() cinv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("fetching inverse from cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
